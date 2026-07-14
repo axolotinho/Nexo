956674e22655fd60a4997a2089b25ddfbd527247
Jose Leandro Gomes Pereira <duq0233178@aluno.firjansenaisesi.com.br>
2026-07-14 13:09:18 -0300

    updated

--- src/pages/Calendar/index.jsx
+++ src/pages/Calendar/index.jsx
@@ -1,8 +1,85 @@
+import { useState, useEffect} from 'react' 
+import { useNavigate } from "react-router-dom";
+import api from '../../services/api'
+import './style.css'
+
 export default function Calendar() {
-  console.log('to aqui Pai')
+  const title = "Calendário"
+  const hoje = new Date();
+  const dia = hoje.getDate();
+  const mes = hoje.toLocaleString("pt-BR", {
+    month: "long",
+  });
+  const diasNoMes = new Date(
+    hoje.getFullYear(),
+    hoje.getMonth() + 1,
+    0
+  ).getDate();
+  const dias = Array.from(
+    { length: diasNoMes },
+    (_, i) => i + 1
+  );
+  const navigate = useNavigate();
+  const [card, setCard] = useState([])
+  async function getCard(){
+    const cardFromApi = await api.get('/card')
+
+    setCard(cardFromApi.data);
+  }
+
+  useEffect (() =>  {
+    const token = localStorage.getItem("token");
+    
+    if (!token) {
+      navigate("/");
+    }
+
+    getCard();
+  }, []);
+
   return (
     <div>
-      <h1>Pai, afasta de mim esse cálice</h1>
+      <div className="background">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="header">
+        <h2>
+          {title.split("").map((letter, index) => (
+            <span key={index}>{letter}</span>
+          ))}
+        </h2>
+      </div>
+      <div className="calendar">
+        <h3>{mes}</h3>
+
+        <div className="calendar-grid">
+          {dias.map((dia) => (
+            <div key={dia} className="day">
+
+              <span className="day-number">{dia}</span>
+
+              {card
+                .filter(
+                  (ca) => new Date(ca.data_entrega).getDate() === dia
+                )
+                .map((ca) => (
+                  <div key={ca.id} className="card">
+                    <span>{ca.title}</span>
+                  </div>
+                ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='rodape'>
+        <button onClick={() => navigate("/Home")}><i class="fa-solid fa-house"></i></button>
+        <button className='active'><i class="fa-solid fa-calendar"></i></button>
+        <button onClick={() => navigate("/Chat")}><i class="fa-solid fa-comment-dots"></i></button>
+        <button onClick={() => navigate("/Task")}>{dia}</button>
+        <button onClick={() => navigate("/Ia")}><i class="fa-solid fa-dove"></i></button>
+      </div>
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,112 @@
+import { useState, useEffect} from 'react' 
+import { useNavigate } from "react-router-dom";
+import api from '../../services/api'
+import './style.css'
+
 export default function Home() {
-  console.log('to aqui Pai')
+  const title = "Kanban"
+  const description = "Veja como está indo o trabalho em equipe"
+  const dia = new Date().getDate();
+  const navigate = useNavigate();
+  const [kanban, setKanban] = useState([])
+  const [card, setCard] = useState([])
+  async function getKanban(){
+    const kanbanFromApi = await api.get('/kanban')
+    setKanban(kanbanFromApi.data);
+  }
+
+  async function getCard(){
+    const cardFromApi = await api.get('/card')
+
+    setCard(cardFromApi.data);
+  }
+
+  useEffect (() =>  {
+    const token = localStorage.getItem("token");
+    
+    if (!token) {
+      navigate("/");
+    }
+    getKanban();
+    getCard();
+  }, []);
+
   return (
     <div>
-      <h1>Pai, afasta de mim esse cálice</h1>
+      <div className="background">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className='topo'>
+        <div className="header">
+          <h2>
+            {title.split("").map((letter, index) => (
+              <span key={index}>{letter}</span>
+            ))}
+          </h2>
+
+          <h3>
+            {description.split("").map((letter, index) => (
+              <span key={index}>
+                {letter === " " ? "\u00A0" : letter}
+              </span>
+            ))}
+          </h3>
+        </div>
+        <div className="rodape">
+          <button className="active">
+            <i className="fa-solid fa-house"></i>
+          </button>
+
+          <button onClick={() => navigate("/Calendar")}>
+            <i className="fa-solid fa-calendar"></i>
+          </button>
+
+          <button onClick={() => navigate("/Chat")}>
+            <i className="fa-solid fa-comment-dots"></i>
+          </button>
+
+          <button onClick={() => navigate("/Task")}>
+            {dia}
+          </button>
+
+          <button onClick={() => navigate("/Ia")}>
+            <i className="fa-solid fa-dove"></i>
+          </button>
+        </div>
+      </div>
+      <div className="layout">
+
+        <div className="kanban">
+          {kanban.map((kan) => (
+            <div 
+              key={kan.id}
+              className="kanban-column"
+              style={{ border: `2px solid ${kan.color}` }}
+            >
+              <div 
+                className="column-title"
+                style={{background: kan.color}}
+              >
+                <h3>{kan.name}</h3>
+              </div>
+
+              <div className="cards">
+                {card.map((obj) =>
+                  obj.kanban_id === kan.id ? (
+                    <div key={obj.id} className="task-card">
+                      <h4>{obj.title}</h4>
+                      <p>{obj.description}</p>
+                    </div>
+                  ) : null
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </div>
     </div>
   );
 }
